a82642b20ebff3fa75f986a3de88e01a7f9bd5f8
Salem Hilal <bichiliad@gmail.com>
2012-10-02 16:22:04 -0400

    Cookin' with bootstrap. Also, some HTML5 compliance stuff.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){
 
-
     $.getJSON("http://api.feedzilla.com/v1/categories/26/articles.json", function(data){
         var articleTemplate = new Template("/templates/article");
 
-        console.log(data);
         data.articles.forEach(function(c){
             articleTemplate.append(c, "#articles");
         })
     });
 
-    
 });
