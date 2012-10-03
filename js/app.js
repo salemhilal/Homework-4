48617845fc8992190103f9a9f8cb5157aef955dc
Salem Hilal <bichiliad@gmail.com>
2012-10-03 00:24:35 -0400

    Added isotope and ascensor. Cooking with fire, we are.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
 
+
     $.getJSON("http://api.feedzilla.com/v1/categories/26/articles.json", function(data){
         var articleTemplate = new Template("/templates/article");
 
+
         data.articles.forEach(function(c){
             articleTemplate.append(c, "#articles");
-        })
+        });
+
+        //ISOTOPE UP IN THIS B*TCH
+        $("#articles").isotope({
+            itemSelector : '.article',
+            layoutMode : 'fitRows'
+        });
+
+        //
+        showThatStuff();
     });
+
+
+    function showThatStuff(){
+        //DO NOTHING. For now.
+    }
 
 });
